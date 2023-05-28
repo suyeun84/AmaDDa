eb361f28266821776768f6c91ab6c45cd1b5b534
blueme0 <81468180+blueme0@users.noreply.github.com>
2023-05-28 18:31:16 +0900

    Revert "Merge pull request #1 from taeseung-yoo/subscribe"
    This reverts commit 92fc9b94a56a17dde72b2e1b82ad2a4fdcdbb580.

--- app/src/main/java/com/example/amadda/MainActivity.kt
+++ app/src/main/java/com/example/amadda/MainActivity.kt
@@ -2,53 +2,14 @@ package com.example.amadda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
 import com.example.amadda.databinding.ActivityLoginBinding
-import com.example.amadda.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    lateinit var bnv: BottomNavigationView
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        bnv = binding.bottomNav
 
-        init()
-    }
-
-    private fun init() {
-        supportFragmentManager.beginTransaction().replace(binding.frameLayout.id, SubscribeFragment())
-            .commitAllowingStateLoss()
-
-        bnv.setOnItemSelectedListener { item ->
-            changeFragment(
-                when (item.itemId) {
-                    R.id.calendarMenu -> {
-                        SubscribeFragment()
-                    }
-                    R.id.favoriteMenu -> {
-                        SubscribeFragment()
-                    }
-                    R.id.subscribeMenu -> {
-                        SubscribeFragment()
-                    }
-                    else -> {
-                        SubscribeFragment()
-                    }
-                }
-            )
-            true
-        }
-    }
-
-    fun changeFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(binding.frameLayout.id, fragment)
-            .commit()
     }
 }
