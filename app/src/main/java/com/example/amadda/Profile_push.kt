235f359068f0b4c176dca19a8d6d713f1f53f062
binllionaire <bug20474@gmail.com>
2023-06-05 15:14:02 +0900

    publishig : 푸시 알림 설정 화면

---
+++ app/src/main/java/com/example/amadda/Profile_push.kt
@@ -0,0 +1,11 @@
+package com.example.amadda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile_push : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_push)
+    }
+}
