130db90e880ec0940a23e45e6f3401ff0b4cf73d
suyeun84 <kelsey6225@naver.com>
2023-06-05 22:21:44 +0900

    calender fix

--- app/src/main/java/com/example/amadda/CalendarFragment.kt
+++ app/src/main/java/com/example/amadda/CalendarFragment.kt
@@ -36,6 +36,7 @@ class CalendarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCalendarBinding.inflate(inflater, container, false)
+<<<<<<< Updated upstream
 //        val intent = Intent(context, CalendarActivity::class.java)
 //        startActivity(intent)
         return binding!!.root
@@ -51,12 +52,50 @@ class CalendarFragment : Fragment() {
                 7,
                 StaggeredGridLayoutManager.VERTICAL
             )
+=======
+        return binding.root
+//        val view =  inflater.inflate(R.layout.fragment_calendar, container, false)
+//        return view
+    }
+
+    private fun initDate() {
+        val date: LocalDate = LocalDate.now()
+        val iyear: Int = date.year
+        val imonth: Int = date.monthValue
+        dateModel.setCurYear(iyear)
+        dateModel.setCurMonth(imonth)
+        println("initdate! $iyear / $imonth")
+
+//        binding.prevMonth.setOnClickListener {
+//            initCalendar()
+//        }
+//
+//        binding.nextMonth.setOnClickListener {
+//            initCalendar()
+//            adapter_calendar = CalendarRecyclerAdapter(monthData)
+//            binding.recyclerViewCalendar.adapter = adapter_calendar
+//        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+        println("datemodel : ${dateModel.curYear.value} / ${dateModel.curMonth.value}")
+        year = dateModel.curYear.value!!
+        month = dateModel.curMonth.value!!
+
+        binding.recyclerViewCalendar.layoutManager = StaggeredGridLayoutManager(
+            7,
+            StaggeredGridLayoutManager.VERTICAL
+>>>>>>> Stashed changes
         )
-        val calendar = GregorianCalendar(year, month, 1)
+//        val calendar = GregorianCalendar(year, month, 1)
 
-        val dayOfWeek: Int = calendar.get(Calendar.DAY_OF_WEEK) - 1 // 1일의 위치
-        val max: Int = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+        //여기 코드 실행하면 7월꺼 출력됨
+//        val dayOfWeek: Int = calendar.get(Calendar.DAY_OF_WEEK) - 1 // 1일의 위치
+//        val max: Int = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
 
+<<<<<<< Updated upstream
 //        var str = ""
 
 //        binding!!.textViewDate.setText(
@@ -87,6 +126,18 @@ class CalendarFragment : Fragment() {
 
             //날짜에 따른 일정 표시
         }
+=======
+//        for (i in 0 until dayOfWeek) {
+//            monthData.add("0")
+//        }
+//        for (i in 1..max) {
+//            var mdate = Integer.toString(year * 10000 + (month + 1) * 100 + i)
+//            Log.d("mdate", mdate)
+//
+//            monthData.add(mdate)
+//            //날짜에 따른 일정 표시
+//        }
+>>>>>>> Stashed changes
         adapter_calendar = CalendarRecyclerAdapter(monthData)
         binding!!.recyclerViewCalendar.adapter = adapter_calendar
 
