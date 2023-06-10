5398a9f33b430f6102718de99a99f44e45e92f8a
suyeun84 <kelsey6225@naver.com>
2023-06-11 01:25:18 +0900

    bookmark

--- app/src/main/java/com/example/amadda/BookMarkFragment.kt
+++ app/src/main/java/com/example/amadda/BookMarkFragment.kt
@@ -8,20 +8,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.GenericTypeIndicator
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class BookMarkFragment : Fragment() {
-    private lateinit var BookMarkAdapter: BookMarkAdapter
+    lateinit var rdb: DatabaseReference
+    private lateinit var bookmarkAdapter: BookMarkAdapter
     private var columnCount = 1
-    private var arrayList = arrayListOf<EventData>(
-        EventData("건국대 학사일정", "개교기념일", 5, false),
-        EventData("KBO리그", "LG vs SSG", 20, false),
-        EventData("프리미어리그", "토트넘 vs 첼시", 25, false)
-    )
+    private var arrayList = arrayListOf<EventData>()
+
+    val userId: String = "kelsey6225"
 
     @SuppressLint("CutPasteId")
     override fun onCreateView(
@@ -29,35 +33,91 @@ class BookMarkFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        BookMarkAdapter = BookMarkAdapter(arrayList)
         val view =  inflater.inflate(R.layout.fragment_book_mark_list, container, false)
-        val addBookMark = view.findViewById<ImageView>(R.id.addBookMark)
-        val eraseBookMark = R.layout.fragment_book_mark
-        val recyclerList = view.findViewById<RecyclerView>(R.id.recyclerList)
-        recyclerList.adapter = BookMarkAdapter
-        val size = arrayList.size
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerList)
-        if (recyclerView is RecyclerView) {
-            with(recyclerView) {
-                setHasFixedSize(true)
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
+        rdb = Firebase.database.getReference("Users/user/" + userId)
+        rdb.child("bookmarkList").get().addOnSuccessListener { dataSnapshot ->
+            GlobalScope.launch(Dispatchers.Main) {
+                // 비동기 작업이 완료된 후에 실행될 코드
+                if (dataSnapshot.exists()) {
+                    val listType = object : GenericTypeIndicator<ArrayList<EventData>>() {}
+                    val subArr = dataSnapshot.getValue(listType)
+
+                    if (subArr != null) {
+                        for (i in subArr.indices) {
+//                            Log.d("bookmarkList", subArr[i].toString())
+                            val event = EventData(subArr[i].category.toString(), subArr[i].event.toString(), subArr[i].Dday.toInt())
+                            arrayList.add(event)
+                        }
+                    }
+                    bookmarkAdapter = BookMarkAdapter(arrayList)
+                    val recyclerList = view.findViewById<RecyclerView>(R.id.recyclerList)
+                    recyclerList.adapter = bookmarkAdapter
+
+                    val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerList)
+                    if (recyclerView is RecyclerView) {
+                        with(recyclerView) {
+                            setHasFixedSize(true)
+                            layoutManager = when {
+                                columnCount <= 1 -> LinearLayoutManager(context)
+                                else -> GridLayoutManager(context, columnCount)
+                            }
+                            adapter = BookMarkAdapter(arrayList)
+                        }
+                    }
                 }
-                adapter = BookMarkAdapter(arrayList)
+                val addBookMark = view.findViewById<ImageView>(R.id.addBookMark)
+                val size = arrayList.size
+                Log.d("bookmarkList", size.toString())
+
+                addBookMark.setOnClickListener {
+                    for (i in 0 until size){
+                        val category = arrayList[i].category.toString()
+                        val event = arrayList[i].event.toString()
+                        var dDay = arrayList[i].Dday.toString().toInt()
+                        val edit = !arrayList[i].edit
+
+                        val eventdata = EventData(category, event, dDay, edit)
+                        arrayList[i] = eventdata
+                    }
+
+                    bookmarkAdapter = BookMarkAdapter(arrayList)
+                    val recyclerList = view.findViewById<RecyclerView>(R.id.recyclerList)
+                    recyclerList.adapter = bookmarkAdapter
+                    val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerList)
+                    if (recyclerView is RecyclerView) {
+                        with(recyclerView) {
+                            setHasFixedSize(true)
+                            layoutManager = when {
+                                columnCount <= 1 -> LinearLayoutManager(context)
+                                else -> GridLayoutManager(context, columnCount)
+                            }
+                            adapter = BookMarkAdapter(arrayList)
+                        }
+                    }
+                }
+
+
+
+//                val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerList)
+//                recyclerView.adapter = bookmarkAdapter
             }
         }
 
-        addBookMark.setOnClickListener {
-            for (i in 0 until size){
-                val category = arrayList[i].category
-                val event = arrayList[i].event
-                val dday = arrayList[i].Dday
-                arrayList[i] = EventData(category, event, dday, true)
+        bookmarkAdapter = BookMarkAdapter(arrayList)
+        val recyclerList = view.findViewById<RecyclerView>(R.id.recyclerList)
+
+        bookmarkAdapter.itemClickListener = object : BookMarkAdapter.OnItemClickListener{
+            override fun onItemClick(data: EventData, position: Int) {
+                Log.d("bookmarkList", "click")
+                arrayList.removeAt(position)
+                Log.d("bookmarkList", position.toString())
+//                bookmarkAdapter.notifyItemRemoved(position)
+                bookmarkAdapter.notifyDataSetChanged()
             }
-            BookMarkAdapter.setData(arrayList)
         }
+        recyclerList.adapter = bookmarkAdapter
+
         return view
     }
 }
--- app/src/main/java/com/example/amadda/EventData.kt
+++ app/src/main/java/com/example/amadda/EventData.kt
@@ -1,3 +1,10 @@
 package com.example.amadda
 
-data class EventData(val category:String, val event:String, val Dday:Int, var edit:Boolean=false)
+data class EventData(
+    val category: String,
+    val event: String,
+    val Dday: Int,
+    var edit: Boolean = false
+){
+    constructor() : this("pw","",0,false)
+}
