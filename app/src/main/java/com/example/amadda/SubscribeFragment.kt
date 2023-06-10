9df9b8118ce47c52efdd63404dcd098d4c9b37b8
blueme0 <ksytpdus0304@gmail.com>
2023-06-10 21:34:07 +0900

    subscribe firebase랑 연결

--- app/src/main/java/com/example/amadda/SubscribeFragment.kt
+++ app/src/main/java/com/example/amadda/SubscribeFragment.kt
@@ -11,8 +11,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.amadda.databinding.ActivityMainBinding
 import com.example.amadda.databinding.FragmentSubscribeBinding
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.GenericTypeIndicator
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class SubscribeFragment : Fragment() {
 
@@ -23,25 +28,43 @@ class SubscribeFragment : Fragment() {
     lateinit var rdb: DatabaseReference
     lateinit var adapter: SubscribeAdapter
     val userId: String = "blueme0"
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        data.add(Subscription("건국대 학사일정", R.drawable.subscribe_logo_1, true))
-        data.add(Subscription("프리미어리그", R.drawable.subscribe_logo_2, true))
+        data.add(Subscription("건국대 학사일정", R.drawable.subscribe_logo_1, false))
+        data.add(Subscription("프리미어리그", R.drawable.subscribe_logo_2, false))
         data.add(Subscription("KBO리그", R.drawable.subscribe_logo_3, false))
-        data.add(Subscription("페스티벌", R.drawable.subscribe_logo_4, true))
-        data.add(Subscription("장학금", R.drawable.subscribe_logo_5, false))
-        data.add(Subscription("공모전", R.drawable.subscribe_logo_2, false))
-
-
-
+        data.add(Subscription("페스티벌", R.drawable.subscribe_logo_4, false))
         binding = FragmentSubscribeBinding.inflate(inflater, container, false)
         mbinding = ActivityMainBinding.inflate(inflater, container, false)
-        initRecyclerView()
+
+        rdb = Firebase.database.getReference("Users/user/" + userId)
+        rdb.child("subscribe").get().addOnSuccessListener { dataSnapshot ->
+            GlobalScope.launch(Dispatchers.Main) {
+                // 비동기 작업이 완료된 후에 실행될 코드
+                if (dataSnapshot.exists()) {
+                    val listType = object : GenericTypeIndicator<ArrayList<Int>>() {}
+                    val subArr = dataSnapshot.getValue(listType)
+
+                    if (subArr != null) {
+                        for (i in subArr.indices) {
+                            val index = subArr[i]
+                            if (index != null && index < data.size) {
+                                data[index].isSubscribing = true
+                            }
+                        }
+
+                        rdb.child("subscribe").setValue(subArr)
+                    }
+                }
+                initRecyclerView() // 비동기 작업이 완료된 후에 initRecyclerView()를 호출
+            }
+        }
         return binding.root
     }
-
+    
     private fun initRecyclerView() {
         binding.apply {
             subscribeRecyclerView.layoutManager = LinearLayoutManager(context,
@@ -49,33 +72,60 @@ class SubscribeFragment : Fragment() {
             adapter = SubscribeAdapter(data)
             adapter.itemClickListener = object : SubscribeAdapter.OnItemClickListener {
                 override fun OnItemClick(data: Subscription, position: Int) {
-                    data.isSubscribing = !data.isSubscribing
+                    rdb = Firebase.database.getReference("Users/user/" + userId)
                     if (data.isSubscribing) {
-                        rdb = Firebase.database.getReference("Users/user/" + userId)
-                        rdb.child("subscribe").get().addOnSuccessListener {
-//                            it.value.toString()
-                            var subArr: ArrayList<Int> = it.value as ArrayList<Int>
-                            Log.d("initRecyclerViewrdb", "it value class : ${(it.value)!!::class}")
-                            Log.d("initRecyclerViewrdb", "subArr class : ${ subArr::class}")
-//                            subArr.remove(subArr.indexOf(position))
-                            Log.d("initRecyclerViewrdb", "it value : ${it.value}")
-                            Log.d("initRecyclerViewrdb", "position is ... $position")
-                            Log.d("initRecyclerViewrdb", subArr.toString())
-                            Log.d("initRecyclerViewrdb", subArr.indexOf(0).toString())
-                            rdb.child("/subscribe").setValue(subArr)
-//                            Log.d("initRecyclerViewrdb", subArr.toString())
+                        data.isSubscribing = false
+                        rdb.child("subscribe").get().addOnSuccessListener { dataSnapshot ->
+                            if (dataSnapshot.exists()) {
+                                val listType = object : GenericTypeIndicator<ArrayList<Int>>() {}
+                                val subArr = dataSnapshot.getValue(listType)
+
+                                if (subArr != null) {
+                                    subArr.remove(position)
+                                    saveSubscription(subArr) { success ->
+                                        if (success) {
+                                            // 데이터베이스에 변경된 값 저장 후 처리할 작업 수행
+                                        } else {
+                                            // 저장 실패 처리
+                                        }
+                                    }
+                                }
+                            }
                         }
-//                        Log.d("initRecyclerViewrdb", str.toString())
-
-
-
-//                        rdb = Firebase.database.getReference("Users/user/" + binding.editTextId.text.toString() + "/categoryList")
-//                        userinfo.category = cateArr
-//                        rdb.child("category").setValue(userinfo.category)
+                    } else {
+                        data.isSubscribing = true
+                        rdb.child("subscribe").get().addOnSuccessListener { dataSnapshot ->
+                            if (dataSnapshot.exists()) {
+                                val listType = object : GenericTypeIndicator<ArrayList<Int>>() {}
+                                val subArr = dataSnapshot.getValue(listType)
+                                if (subArr != null) {
+                                    if (!subArr.contains(position)) {
+                                        subArr.add(position)
+                                        saveSubscription(subArr) { success ->
+                                            if (success) {
+                                                // 데이터베이스에 변경된 값 저장 후 처리할 작업 수행
+                                            } else {
+                                                // 저장 실패 처리
+                                            }
+                                        }
+                                    }
+                                }
+                            } else {
+                                val subArr = ArrayList<Int>()
+                                subArr.add(position)
+                                saveSubscription(subArr) {success ->
+                                    if (success) {
+                                        // 데이터베이스에 변경된 값 저장 후 처리할 작업 수행
+                                        Log.d("saveSubscription", "canceled success")
+                                    } else {
+                                        // 저장 실패 처리
+                                        Log.d("saveSubscription", "canceled failed")
+                                    }
+                                }
+                            }
+                        }
                     }
-                    else {
 
-                    }
                     adapter.notifyItemChanged(position)
                 }
             }
@@ -83,4 +133,14 @@ class SubscribeFragment : Fragment() {
         }
     }
 
+    private fun saveSubscription(subArr: ArrayList<Int>, completion: (Boolean) -> Unit) {
+        rdb.child("subscribe").setValue(subArr)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    completion(true)
+                } else {
+                    completion(false)
+                }
+            }
+    }
 }
