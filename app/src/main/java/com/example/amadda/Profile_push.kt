adfc50abdd7f64c4bdc7801d794fa20f8e403b42
binllionaire <bug20474@gmail.com>
2023-06-11 13:41:28 +0900

    feat : TODO 바텀시트 추가중

--- app/src/main/java/com/example/amadda/Profile_push.kt
+++ app/src/main/java/com/example/amadda/Profile_push.kt
@@ -2,10 +2,22 @@ package com.example.amadda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.amadda.databinding.ActivityProfileBinding
+import com.example.amadda.databinding.ActivityProfilePushBinding
+import com.example.amadda.databinding.ActivityTimeTableBinding
 
 class Profile_push : AppCompatActivity() {
+    lateinit var binding: ActivityProfilePushBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_push)
+        binding = ActivityProfilePushBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
     }
+    fun init(){
+        binding.backBtnAppbar.setOnClickListener {
+            finish()
+        }
+    }
+
 }
