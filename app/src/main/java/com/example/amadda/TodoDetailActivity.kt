1043153b54fffcbb8fa7b1b818fb20dae684563e
blueme0 <ksytpdus0304@gmail.com>
2023-06-21 01:55:38 +0900

    merge :

--- app/src/main/java/com/example/amadda/TodoDetailActivity.kt
+++ app/src/main/java/com/example/amadda/TodoDetailActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Paint
 import android.net.Uri
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
